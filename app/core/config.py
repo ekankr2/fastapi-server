2141dd6ca30d41ed1c67502704af2d841aa88922
ekankr2 <ekankr2@naver.com>
2022-11-22 20:30:21 +0900

    config: make env environments, set it to settings()

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,14 @@ class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
     ENVIRONMENT: str = os.getenv('ENVIRONMENT')
     DB_URL: str = os.getenv('DB_URL')
+    API_PREFIX: str = os.getenv('API_PREFIX')
+    DATABASE_HOST: str = os.getenv('DATABASE_HOST')
+    DATABASE_PORT: str = os.getenv('DATABASE_PORT')
+    DATABASE_USERNAME: str = os.getenv('DATABASE_USERNAME')
+    DATABASE_PASSWORD: str = os.getenv('DATABASE_PASSWORD')
+    DATABASE_NAME: str = os.getenv('DATABASE_NAME')
+    JWT_SECRET_ACCESS_KEY: str = os.getenv('JWT_SECRET_ACCESS_KEY')
+    JWT_SECRET_REFRESH_KEY: str = os.getenv('JWT_SECRET_REFRESH_KEY')
 
     class Config:
         env_file = ".env.local"
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
 app = FastAPI(
-    title='FastAPI-server'
+    title='FastAPI-server',
+    root_path=settings.API_PREFIX
 )
 
 
