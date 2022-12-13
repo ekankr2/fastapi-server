16538a1a5bdcd21fbd8f5df7c92d1c0f0836bc62
ekankr2 <ekankr2@naver.com>
2022-12-13 20:47:51 +0900

    feat: import post_comment class in base.py

--- app/db/base.py
+++ app/db/base.py
@@ -2,4 +2,5 @@
 # imported by Alembic
 from app.db.base_class import Base  # noqa
 from app.domain.post import Post # noqa
+from app.domain.post_comment import PostComment # noqa
 from app.domain.user import User # noqa
