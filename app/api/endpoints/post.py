d72b6b5e89831a5c47b57865990a64cfa72df371
ekankr2 <ekankr2@naver.com>
2022-12-28 18:57:54 +0900

    feat: create base crud service

--- app/api/endpoints/post.py
+++ app/api/endpoints/post.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import Depends, status, APIRouter
 from fastapi_restful.cbv import cbv
 from sqlalchemy.orm import Session
@@ -20,4 +22,8 @@ class PostController:
     @router.post('/', response_model=schemas.Post, status_code=status.HTTP_201_CREATED)
     def create(self, request: schemas.PostCreate, current_user: User = Depends(dependencies.get_current_user)):
         return self.post_service.create_post(db=self.db, request=request, user_id=current_user.id)
+
+    @router.get('/', response_model=List[schemas.Post], summary="Get List of Posts")
+    def get_posts(self, skip: int = 0, limit: int = 100, current_user: User = Depends(dependencies.get_current_user)):
+        return 'ok'
 
