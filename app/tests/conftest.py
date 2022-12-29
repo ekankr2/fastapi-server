7ddcce70f9b59c7b06d336789be2954193fdef9f
ekankr2 <ekankr2@naver.com>
2022-12-29 18:14:41 +0900

    test: working to set test db

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,17 +2,28 @@ from typing import Dict, Generator
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 from app.core.config import settings
 from app.db.session import SessionLocal
 from app.main import app
 
+DATABASE_URI= 'sqlite:///./test.db'
+
 
 @pytest.fixture(scope="session")
 def db() -> Generator:
-    yield SessionLocal()
+    test_engine = create_engine(DATABASE_URI, pool_pre_ping=True)
+    TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+    Base = declarative_base()
+    Base.metadata.create_all(test_engine)
+    yield TestSessionLocal()
+    Base.metadata.drop_all()
 
+# @pytest.fixture(scope="session")
+# def db() -> Generator:
+#     yield SessionLocal()
 
 @pytest.fixture(scope="module")
 def client() -> Generator:
