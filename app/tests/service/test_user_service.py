44a280a25ab79d006353d87a2fed71cbac939d7a
ekankr2 <ekankr2@naver.com>
2023-01-02 21:05:54 +0900

    test: get user, get user by email

--- app/tests/service/test_user_service.py
+++ app/tests/service/test_user_service.py
@@ -1,3 +1,4 @@
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from app.schemas import UserCreate
@@ -13,3 +14,27 @@ def test_create_user(db: Session) -> None:
     user = user_service.create_user(db, request=user_in)
     assert user.email == email
     assert hasattr(user, "hashed_password")
+
+
+def test_get_user(db: Session):
+    email = random_email()
+    name = 'random_name'
+    password = random_lower_string()
+    user_in = UserCreate(email=email, password=password, name=name)
+    user = user_service.create_user(db, request=user_in)
+    user_2 = user_service.get(db, id=user.id)
+    assert user_2
+    assert user.email == user_2.email
+    assert jsonable_encoder(user) == jsonable_encoder(user_2)
+
+
+def test_get_user_by_email(db: Session):
+    email = random_email()
+    name = 'random_name'
+    password = random_lower_string()
+    user_in = UserCreate(email=email, password=password, name=name)
+    user = user_service.create_user(db, request=user_in)
+    user_2 = user_service.get_by_email(db, email=user.email)
+    assert user_2
+    assert user.email == user_2.email
+    assert jsonable_encoder(user) == jsonable_encoder(user_2)
