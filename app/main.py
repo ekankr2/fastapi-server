265b32d10984de2f83af8fc86a45ec47a1db7a64
ekankr2 <ekankr2@naver.com>
2023-01-06 15:36:28 +0900

    feat: create logger

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.logger import logger
 
 from app.core.config import settings
 from app.api.router import api_router
+from app.tests.utils.logger import init_loggers
 
 app = FastAPI(
     title='FastAPI Server',
 )
+
+@app.on_event('startup')
+async def startup_event():
+    init_loggers()
+    logger.info('helloworld')
+    logger.info('helloworld')
+    logger.info('helloworld')
+    logger.warning('warn')
 
 
 app.include_router(api_router, prefix=settings.API_PREFIX)
