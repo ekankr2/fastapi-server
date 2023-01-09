f1e137ec16ee71d57da7bb334c7d51d2962e30be
ekankr2 <ekankr2@naver.com>
2023-01-09 17:28:25 +0900

    feat: add colors to logger

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from fastapi.logger import logger
-
 from app.core.config import settings
 from app.api.router import api_router
 from app.tests.utils.logger import init_loggers
@@ -9,13 +7,9 @@ app = FastAPI(
     title='FastAPI Server',
 )
 
+
 @app.on_event('startup')
 async def startup_event():
     init_loggers()
-    logger.info('helloworld')
-    logger.info('helloworld')
-    logger.info('helloworld')
-    logger.warning('warn')
-
 
 app.include_router(api_router, prefix=settings.API_PREFIX)
